d250ed6cfff1c3134e891e00905c1c1e1031f202
bberickson@gmail.com <bberickson@gmail.com>
2025-12-31 11:35:13 -0700

    size results update

--- workflow/scripts/fragment_results.R
+++ workflow/scripts/fragment_results.R
@@ -2,14 +2,20 @@
 
 library("tidyverse")
 
-num_cols <- count_fields(snakemake@input[[1]],
+args = commandArgs(trailingOnly=TRUE)
+
+frag_files <- strsplit(args[1], " ")[[1]]
+num_cols <- count_fields(frag_files[1],
                          n_max = 1,
-             tokenizer = tokenizer_tsv())
+                         tokenizer = tokenizer_tsv())
+samp <- args[2]
+out_file <- args[3]
+
 frag <- NULL
 if(num_cols > 2){
-  samp <- paste(snakemake@params[["samp"]],c("Frag_Len_Mean", "Read_Len_Mean"),sep = "_")
-  for(i in seq_along(snakemake@input)){
-    frag_temp <- read_tsv(snakemake@input[[i]], show_col_types = FALSE) %>% 
+  samp <- paste(samp,c("Frag_Len_Mean", "Read_Len_Mean"),sep = "_")
+  for(i in seq_along(frag_files)){
+    frag_temp <- read_tsv(frag_files[i], show_col_types = FALSE) %>% 
       dplyr::rename(sample=`...1`) %>% 
       dplyr::select(sample,`Frag. Len. Mean`, `Read Len. Mean`) %>%
       dplyr::mutate(`Frag. Len. Mean` = round(`Frag. Len. Mean`,digits = 5),
@@ -18,22 +24,22 @@ if(num_cols > 2){
     
     if(str_detect(frag_temp$sample,"/bams/")){
       frag_temp <- frag_temp %>% separate(sample,into=c("dir","sample"),sep="/bams/") %>%
-        separate(sample,into=c("sample","file"),sep=paste0("_", snakemake@params[["samp"]])) %>%
+        separate(sample,into=c("sample","file"),sep=paste0("_", samp)) %>%
         dplyr::select(-dir,-file)
     }
     frag <- bind_rows(frag,frag_temp)
   }
 } else {
-  samp <- paste(snakemake@params[["samp"]],c("Read_Len_Mean", "Read_Len_Median"),sep = "_")
-  for(i in seq_along(snakemake@input)){
-    samp_name <- snakemake@input[[i]] %>% str_replace(., ".*/", "") %>% 
-      str_remove(.,paste0("_aligned_", snakemake@params[["samp"]],"_fragment.txt"))
+  samp <- paste(samp,c("Read_Len_Mean", "Read_Len_Median"),sep = "_")
+  for(i in seq_along(frag_files)){
+    samp_name <- frag_files[i] %>% str_replace(., ".*/", "") %>% 
+      str_remove(.,paste0("_aligned_", samp,"_fragment.txt"))
     
-    frag_temp <- read_tsv(snakemake@input[[i]], show_col_types = FALSE, col_names = F, skip = 4, n_max = 2) %>% 
+    frag_temp <- read_tsv(frag_files[i], show_col_types = FALSE, col_names = F, skip = 4, n_max = 2) %>% 
       mutate(sample=samp_name) %>% pivot_wider(values_from =X2,names_from = X1) %>% 
       dplyr::rename(!!samp[1] := `#Avg:`, !!samp[2] := `#Median:`)
     frag <- bind_rows(frag,frag_temp)
   }
 }
 
-write_tsv(frag, snakemake@output[[1]])
+write_tsv(frag, out_file)
